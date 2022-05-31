b623cf9778374cfe1eef0791a31ce8864707d1e7
Suraj-bhan <2018mmb1296@iitrpr.ac.in>
2022-05-31 10:41:28 +0530

    first commit with header

---
+++ components/header/index.tsx
@@ -0,0 +1,17 @@
+
+import styles from './Header.module.css'
+
+export default function Header() {
+  return (
+    <div className='shadow-md bg-white h-16 flex justify-between'>
+        {/* <div className=""> */}
+            <div className='text-xl self-center pl-8'>
+                Spend Calculator
+            </div>
+            <div className='self-center pr-8'>
+                Spend Calculator
+            </div>
+        {/* </div> */}
+    </div>
+  )
+}
---
+++ components/sidebar/index.tsx
@@ -0,0 +1,17 @@
+
+import styles from './Header.module.css'
+
+export default function SideBar() {
+  return (
+    <div className='shadow-md bg-white w-80 h-full flex justify-between'>
+        {/* <div className=""> */}
+            <div className='text-xl self-center pl-8'>
+                Spend Calculator
+            </div>
+            <div className='self-center pr-8'>
+                Spend Calculator
+            </div>
+        {/* </div> */}
+    </div>
+  )
+}
