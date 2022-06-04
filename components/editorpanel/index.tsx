34da04e4109d1337ee91e3c6d8e7576c6bbdd049
Suraj-bhan <2018mmb1296@iitrpr.ac.in>
2022-06-04 10:22:07 +0530

    updated editor panel

--- components/editorpanel/index.tsx
+++ components/editorpanel/index.tsx
@@ -1,23 +1,37 @@
-
+import classes from './editorpanel.module.css';
 
 export default function EditorPanel() {
-    return (
-      <div className='shadow-md bg-white w-full mx-8 my-8 flex flex-col-2 justify-evenly'>
-        <div className="flex w-2/5 flex-col space-y-6 my-6">
+  return (
+    <div className='shadow-md bg-white w-full mx-8 my-8 flex flex-col-2 justify-evenly'>
+      <div className="flex w-2/5 flex-col space-y-6 my-6">
+        <div className='flex flex-col'> <span className='text-xs p-1 -mb-2 z-50 bg-white w-20 text-center shadow-md -ml-1 border-2'>Type</span>
           <input type='text' placeholder="Description" className="bg-gray-200 rounded-md p-4" />
-          <input type='number' placeholder="Amount" className="bg-gray-200 rounded-md p-4"/>
-          <select name="cars" id="cars">
-            <option value="volvo">Volvo</option>
-            <option value="saab">Saab</option>
-            <option value="mercedes">Mercedes</option>
-            <option value="audi">Audi</option>
-          </select>
         </div>
-        <div className="flex w-2/5 flex-col space-y-6 my-6">
-          <input type='text' placeholder="Description" className="bg-gray-200 rounded-md p-4"/>
-          <input type='number' placeholder="Amount" className="bg-gray-200 rounded-md p-4"/>
+        <div className='flex flex-col'> <span className='text-xs p-1 -mb-2 z-50 bg-white w-20 text-center shadow-md -ml-1 border-2'>Reason</span>
+          <input type='text' placeholder="Description" className="bg-gray-200 rounded-md p-4" />
         </div>
-       
+        <div className='flex flex-col'> <span className='text-xs p-1 -mb-2 z-50 bg-white w-20 text-center shadow-md -ml-1 border-2'>Medium</span>
+          <input type='text' placeholder="Description" className="bg-gray-200 rounded-md p-4" />
+        </div>
+        <div className='flex flex-col'> <span className='text-xs p-1 -mb-2 z-50 bg-white w-20 text-center shadow-md -ml-1 border-2'>Amount</span>
+          <input type='text' placeholder="Description" className="bg-gray-200 rounded-md p-4" />
+        </div>
       </div>
-    )
-  }
+      <div className="flex w-2/5 flex-col space-y-6 my-6">
+        <div className='flex flex-col'> <span className='text-xs p-1 -mb-2 z-50 bg-white w-20 text-center shadow-md -ml-1 border-2'>Category</span>
+          <input type='text' placeholder="Description" className="bg-gray-200 rounded-md p-4" />
+        </div>
+        <div className='flex flex-col'> <span className='text-xs p-1 -mb-2 z-50 bg-white w-20 text-center shadow-md -ml-1 border-2'>Date</span>
+          <input type='text' placeholder="Description" className="bg-gray-200 rounded-md p-4" />
+        </div>
+        <div className='flex flex-col'> <span className='text-xs p-1 -mb-2 z-50 bg-white w-20 text-center shadow-md -ml-1 border-2'>Time</span>
+          <input type='text' placeholder="Description" className="bg-gray-200 rounded-md p-4" />
+        </div>
+        <div className='flex flex-col'> <span className='text-xs p-1 -mb-2 z-50 bg-white w-20 text-center shadow-md -ml-1 border-2'>Receiver</span>
+          <input type='text' placeholder="Description" className="bg-gray-200 rounded-md p-4" />
+        </div>
+      </div>
+
+    </div>
+  )
+}
