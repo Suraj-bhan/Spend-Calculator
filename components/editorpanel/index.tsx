bbe56c0cd8640caff06757465f4624b71dc3aaff
Suraj-bhan <2018mmb1296@iitrpr.ac.in>
2022-10-31 22:05:01 +0530

    Added editor and package json

--- components/editorpanel/index.tsx
+++ components/editorpanel/index.tsx
@@ -5,7 +5,7 @@ export default function EditorPanel() {
     <div className='shadow-md bg-white w-full mx-8 my-8 flex flex-col-2 justify-evenly'>
       <div className="flex w-2/5 flex-col space-y-6 my-6">
         <div className='flex flex-col'> <span className='text-xs p-1 -mb-2 z-50 bg-white w-20 text-center shadow-md -ml-1 border-2'>Type</span>
-          <input type='text' placeholder="Description" className="bg-gray-200 rounded-md p-4" />
+          <input type='text' placeholder="Debit/Credit" className="bg-gray-200 rounded-md p-4" />
         </div>
         <div className='flex flex-col'> <span className='text-xs p-1 -mb-2 z-50 bg-white w-20 text-center shadow-md -ml-1 border-2'>Reason</span>
           <input type='text' placeholder="Description" className="bg-gray-200 rounded-md p-4" />
@@ -18,7 +18,8 @@ export default function EditorPanel() {
         </div>
       </div>
       <div className="flex w-2/5 flex-col space-y-6 my-6">
-        <div className='flex flex-col'> <span className='text-xs p-1 -mb-2 z-50 bg-white w-20 text-center shadow-md -ml-1 border-2'>Category</span>
+        <div className='flex flex-col'>
+          <span className='text-xs p-1 -mb-2 z-50 bg-white w-20 text-center shadow-md -ml-1 border-2'>Category</span>
           <input type='text' placeholder="Description" className="bg-gray-200 rounded-md p-4" />
         </div>
         <div className='flex flex-col'> <span className='text-xs p-1 -mb-2 z-50 bg-white w-20 text-center shadow-md -ml-1 border-2'>Date</span>
--- pages/editor.tsx
+++ pages/editor.tsx
@@ -2,18 +2,18 @@ import EditorPanel from "../components/editorpanel/index";
 import Header from "../components/header/index";
 import SideBar from "../components/sidebar/index";
 
-export default function Editor(){
-    return (
+export default function Editor() {
+  return (
     <>
-   <div className="h-screen bg-gray-100">
-     <main className='h-5/6'>
-          <Header/>
-        <div className='h-full flex'>
-          <SideBar/>
-          <EditorPanel/>
-        </div>
-      </main>
-   </div>
+      <div className="h-screen bg-gray-100">
+        <main className='h-5/6'>
+          <Header />
+          <div className='h-full flex'>
+            <SideBar />
+            <EditorPanel />
+          </div>
+        </main>
+      </div>
     </>
-    )
+  )
 }
